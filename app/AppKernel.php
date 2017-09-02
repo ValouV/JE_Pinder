441479caace56981356c03c220f891a331722781
Valentin VASSAS <valentin.vassas@epfprojets-sceaux.com>
2017-09-02 19:26:59 +0800

    +gestion users

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new EPFProjets\ProfileBundle\EPFProjetsProfileBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new EPFProjets\UserBundle\EPFProjetsUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
